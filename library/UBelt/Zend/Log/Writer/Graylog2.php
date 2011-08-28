ce503c92ed8fdc18c4fcc393f9174c31cb1dbd72
Amjad Mohamed <andhos@gmail.com>
2011-08-28 21:04:49 +0500

    Added GPL LICENSE.txt file and put copyright notice in all source files

--- library/UBelt/Zend/Log/Writer/Graylog2.php
+++ library/UBelt/Zend/Log/Writer/Graylog2.php
@@ -1,5 +1,31 @@
 <?php
 /**
+ * Graylog2 logger using Zend_Log
+ * The two classes in this package combined will assist you in sending
+ * GELF formatted messages to a Graylog2 server.
+ * 
+ * Copyright (C) 2011  Amjad Mohamed
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @author Amjad Mohamed <andhos@gmail.com>
+ * 
+ */
+
+require_once 'Zend/Log/Writer/Abstract.php';
+
+/**
  * Writes log messages to Graylog2 in GELF format
  * 
  * @author amjad
@@ -7,9 +33,6 @@
  * @depends Zend_Log_Writer_Abstract, UBelt_Zend_Log_Formatter_GELF
  *
  */
-
-require_once 'Zend/Log/Writer/Abstract.php';
-
 class UBelt_Zend_Log_Writer_Graylog2 extends Zend_Log_Writer_Abstract {
 	
 	private $_facility;
